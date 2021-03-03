2c8d4df3dbaf3f99e4062df78b98c76542aa0e46
Paula Rösler <paularoesler@web.de>
2021-03-03 14:19:43 +0100

    further steps auf building a server

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid') // npm install uuid
+uuidv4()
+
 const app = express()
-app.use('/api/users', (req, res) => {
-  res.json([{ name: 'Melissa', role: 'student' }])
+
+const users = []
+
+app.use(express.json()) // add middleware for json data
+
+app.get('/api/users', (req, res) => {
+  res.json(users)
 })
-app.use('/api/cards', (req, res) => {
+
+app.get('/api/users/:id', (req, res) => {
+  const { id } = req.params //destructuring Assignment
+  res.json(users.find(user => user.id === id))
+})
+
+app.post('/api/users', (req, res) => {
+  const newUser = { ...req.body, id: uuidv4() }
+  users.push(newUser)
+  res.json(newUser)
+})
+
+app.get('/api/cards', (req, res) => {
   res.json([{ title: 'First card' }])
 })
+
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000')
 })
