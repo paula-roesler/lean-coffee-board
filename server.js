bb5c3d8ec61f6bec842c8d3c3eeeb11566c3e0d7
Paula Rösler <paularoesler@web.de>
2021-03-03 12:13:59 +0100

    Res in JSON

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 app.use('/api/users', (req, res) => {
-  res.end('[{"name": "Melissa", "role": "student"}]')
+  res.json([{ name: 'Melissa', role: 'student' }])
 })
 app.use('/api/cards', (req, res) => {
-  res.end('[{"title": "First card"}]')
+  res.json([{ title: 'First card' }])
 })
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000')
